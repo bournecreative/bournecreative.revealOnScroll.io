57e6dfe786d9c450beada9e9af985c6bcf0b5e22
Christian John Gonzalez <bournecreative@gmail.com>
2020-11-11 16:34:56 -0800

    another update to update config to get images correctly working for demo

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,19 +23,17 @@ mix.js('src/js/app.js', 'dist/').sass('src/scss/app.scss', 'dist/').setPublicPat
         postCss: [
             require('autoprefixer')
         ],
-        processCssUrls: false
     })
-    // .extract(['jquery', 'scrollmagic', 'gsap'])
     .webpackConfig({
         resolve: {
             alias: {
-                "TweenLite": path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
-                "TweenMax": path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
-                "TimelineLite": path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
-                "TimelineMax": path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
-                "ScrollMagic": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
-                "animation.gsap": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
-                "debug.addIndicators": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js'),
+                'TweenLite': path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
+                'TweenMax': path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
+                'TimelineLite': path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
+                'TimelineMax': path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
+                'ScrollMagic': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
+                'animation.gsap': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
+                'debug.addIndicators': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js'),
             }
         }
     })
